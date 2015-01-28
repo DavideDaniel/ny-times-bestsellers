8a8409776b73f7dac6356851fa31d276b6749fde
DavideDaniel <davidedaniel@gmail.com>
2015-01-28 04:08:21 -0500

    chugging along

--- app.js
+++ app.js
@@ -8,6 +8,39 @@ app.use( bodyParser() );
 app.use( methodOverride( '_method' ) )
 app.use( express.static( __dirname + '/public' ) );
 
-var nytList = JSON.parse(fs.readFileSync( 'nytList.json', 'utf8' ));
+var books = JSON.parse(fs.readFileSync( 'nytList.json', 'utf8' ));
+
+app.get('/books', function ( req, res ) {
+	res.render( 'index.ejs', {
+		books: books
+	} )
+} );
+
+app.get('/book/:id')
 
 
+app.post('/book', function ( req, res ) {
+	var book = {
+		id: counter,
+		title: req.body.title,
+		author: req.body.author,
+		isbn: "",
+		description: ""
+	};
+	books[ counter ] = book;
+	counter++
+	console.log( books.counter );
+	res.method = 'get';
+	fs.writeFile( 'nytList.json', JSON.stringify( books ) )
+	res.redirect( '/books' );
+} );
+
+
+app.put('/book/:id')
+
+
+app.delete('/book/:id')
+
+app.listen( 3000 );
+
+console.log( "Server listening on port: 3000" );
